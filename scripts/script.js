4370daea92d50e5a0dfba582fe4f6f2976f85193
alicia <alicia.lecznar@gmail.com>
2019-07-26 08:35:20 -0400

    final

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+class AddressBook{
+    constructor(){
+        this.contacts = []
+    
+}
+add(name, email, phone, relation){
+    let newContact= new Contact(name, email, phone, relation);
+    this.contacts.push(newContacts);
+}
+ 
+
+delete(name){
+    const index = this.contacts.findIndex(contact =>{
+        return contact.name === name;
+    });
+if (index >= 0){
+    this.contacts.splice(index,1);
+
+
+    }
+}
+print(){
+    for(let i = 0; i<this.contacts.length; i++){
+    console.log("for", this.contacts[i])
+        }
+    }
+}
+
+//for(const contact of this.contacts){
+//  console.log("for of:", contact);
+}
+
+// this.contacts.forEach(contact =>{
+//     console.log('forEach', contact);
+// })
+
+class Contact{
+    constructor(name, email, phone, relation){
+        this.name = name;
+        this.email= email;
+        this.phone= phone;
+        this.relation= relation;
+    }
+    
+}
+
+const addressBook = new AddressBook();
+addressBook.add("Pat","@gmail", "3995873495723", "mom");
+// addressBook.add (newContact("Pat","@gmail", "3995873495723", "mom"));
+addressBook.add("mark", "@gmail", "983473948", "father");
+addressBook.print();
+addressBook.delete ("pat");
+addressBook.print();
+
+
